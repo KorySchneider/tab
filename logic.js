b408d8f6349e2b6af19d577da3ea107833ab5324
Kory Schneider <kory.schneider@gmail.com>
2016-06-11 12:29:44 -0700

    rearranged some stuff

--- logic.js
+++ logic.js
@@ -2,6 +2,47 @@ window.onload = function() {
    updateTime();
 };
 
+/* TODO
+ * options 'screen', similar to help screen
+ * ----------------------------------------
+ * radio button selection of which command should be 'default',
+ * default being the command that runs if you do not give a command
+ */
+
+var comLinks = { 
+   'g':'https://www.google.com/search?q=',
+   'yt':'https://www.youtube.com/results?search_query=',
+   'y':'https://www.youtube.com/results?search_query=',
+   'a':'http://www.amazon.com/s?url=search-alias%3Daps&field-keywords=',
+   'w':'https://www.wikipedia.org/w/index.php?title=Special:Search&search=',
+   'wa':'http://www.wolframalpha.com/input/?i=',
+   't':'',
+}
+
+function interpret() {
+   var input = document.getElementById('input').value;
+   if (input == '') { return; }
+   if (input.trim() == 'help') {
+      displayHelp();
+      return;
+   }
+   if (input.split(';').length == 3 && input.split(';')[2] == 'n') {
+      nt = true;
+   } else {
+      nt = false;
+   }
+   comQry = input.split(';');
+   command = comQry[0].trim();
+   query = comQry[1].trim();
+   if (comLinks.hasOwnProperty(command)) {
+      goToSite(comLinks[command] + query, nt);
+      return false;
+   } else {
+      displayText('command not recognized');
+      return false;
+   }
+}
+
 function verifyKey(e) {
    var keycode;
    if (window.event) {
@@ -35,20 +76,6 @@ function goToSite(url, nt) {
       window.location.replace(url);
       return false;
    }
-}
-
-function checkTime(i) {
-   if (i < 10) { i = '0' + i };
-   if (i == 0) { i = '12' };
-   return i;
-}
-
-function updateTime() {
-   var d = new Date();
-   var h = d.getHours(); if (h > 12) { h -= 12; }; h = checkTime(h);
-   var m = checkTime(d.getMinutes());
-   document.getElementById('time_div').innerHTML = "<p class='timetext'>"+h+':'+m+'</p>';
-   var t = setTimeout(updateTime, 5000);
 }
 
 function displayText(string) {
@@ -114,35 +141,16 @@ function displayHelp() {
    document.getElementById('text_div').innerHTML = helpHTML;
 }
 
-var comLinks = { 
-   'g':'https://www.google.com/search?q=',
-   'yt':'https://www.youtube.com/results?search_query=',
-   'y':'https://www.youtube.com/results?search_query=',
-   'a':'http://www.amazon.com/s?url=search-alias%3Daps&field-keywords=',
-   'w':'https://www.wikipedia.org/w/index.php?title=Special:Search&search=',
-   'wa':'http://www.wolframalpha.com/input/?i=',
-   't':'',
+function checkTime(i) {
+   if (i < 10) { i = '0' + i };
+   if (i == 0) { i = '12' };
+   return i;
 }
-function interpret() {
-   var input = document.getElementById('input').value;
-   if (input == '') { return; }
-   if (input.trim() == 'help') {
-      displayHelp();
-      return;
-   }
-   if (input.split(';').length == 3 && input.split(';')[2] == 'n') {
-      nt = true;
-   } else {
-      nt = false;
-   }
-   comQry = input.split(';');
-   command = comQry[0].trim();
-   query = comQry[1].trim();
-   if (comLinks.hasOwnProperty(command)) {
-      goToSite(comLinks[command] + query, nt);
-      return false;
-   } else {
-      displayText('command not recognized');
-      return false;
-   }
+
+function updateTime() {
+   var d = new Date();
+   var h = d.getHours(); if (h > 12) { h -= 12; }; h = checkTime(h);
+   var m = checkTime(d.getMinutes());
+   document.getElementById('time_div').innerHTML = "<p class='timetext'>"+h+':'+m+'</p>';
+   var t = setTimeout(updateTime, 5000);
 }
