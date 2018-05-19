b39364701a2951b16f12daae94d6600f57296cb0
Kory Schneider <kory.schneider@gmail.com>
2018-05-18 17:42:43 -0700

    Bundle everything to one file

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: __dirname + '/dist',
-    filename: 'index.bundle.js'
+    path: __dirname,
+    filename: 'bundle.js'
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       title: 'taab',
       template: __dirname + '/src/template.html',
-      filename: __dirname + '/dist/index.html',
+      filename: __dirname + '/index.html',
+      inlineSource: '.(js|css)$',
       minify: true
-    })
+    }),
+
+    new HtmlWebpackInlineSourcePlugin()
   ]
 }
