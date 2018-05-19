039fc5c89c33c505c134da0e1185d50fc8e7a117
Kory Schneider <kory.schneider@gmail.com>
2018-05-18 19:07:13 -0700

    Add commands to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { commands } from './commands.js';
-
 // Default config
 let config = {
   defaultCommand: 'g',
@@ -10,7 +8,14 @@ let config = {
   gistID: '',
   links: {},
 }
-
+let aliases = {
+// alias: command
+  'cal': 'gc',
+  'gk': 'k',
+  'ddg': 'dg',
+  'map': 'gm',
+  '?': 'help'
+}
 let newTab = false;
 let lastEnteredCommand = '';
 let messageTimer = null;
@@ -26,19 +31,21 @@ window.onload = () => {
     document.querySelector('#input').focus();
   });
 
+  document.onkeydown = handleKeyDown;
+
   updateClock();
 };
 
 function evaluateInput() {
   let input = document.querySelector('#input').value.trim();
   document.querySelector('#input').value = '';
-  vm.clearMessage();
+  clearMessage();
 
   // Input is empty
   if (input === '') return;
 
   // Save one-line history
-  vm.lastEnteredCommand = input;
+  lastEnteredCommand = input;
 
   // Format input
   let args = input.split(';');
@@ -49,7 +56,7 @@ function evaluateInput() {
 
   // Check if valid command or alias
   const commandList = Object.keys(commands);
-  const aliasList = Object.keys(vm.aliases);
+  const aliasList = Object.keys(aliases);
   let validCommand = false;
   for (let i=0; i < commandList.length; i++) {
     if (command === commandList[i]) {
@@ -58,7 +65,7 @@ function evaluateInput() {
       break;
     } else if (command === aliasList[i]) {
       validCommand = true;
-      command = vm.aliases[command];
+      command = aliases[command];
       args.shift();
       break;
     }
@@ -66,7 +73,7 @@ function evaluateInput() {
 
   // Check if URL
   let isURL = false;
-  if (vm.isURL(args[0])) {
+  if (checkIfURL(args[0])) {
     isURL = true;
     command = args.shift();
   }
@@ -86,13 +93,13 @@ function evaluateInput() {
 
   // Check for newtab flag
   if (args[args.length - 1] === 'n') {
-    vm.newTab = true;
+    newTab = true;
     args.pop(); // remove newtab flag
   }
 
   // Execute
   if (isURL) {
-    vm.redirect(vm.buildURL(command));
+    redirect(buildURL(command));
     return false;
   }
   if (validCommand) {
@@ -101,19 +108,19 @@ function evaluateInput() {
     commands[config['defaultCommand']](args);
   }
   if (validLink) {
-    vm.redirect(config.links[command]);
+    redirect(config.links[command]);
   }
   return false;
 }
 
 // Opens a URL either in current or new tab
 function redirect(url) {
-  if (vm.newTab || config.alwaysNewTab)
+  if (newTab || config.alwaysNewTab)
     window.open(url, '_blank').focus();
   else
     window.location.href = url;
 
-  vm.newTab = false;
+  newTab = false;
   return false;
 }
 
@@ -143,16 +150,16 @@ function displayMessage(msg, timeMs) {
   let msgDiv = document.querySelector('#message');
 
   // Clear existing timer/message
-  if (vm.messageTimer) {
+  if (messageTimer) {
     msgDiv.innerHTML = '';
-    clearTimeout(vm.messageTimer);
+    clearTimeout(messageTimer);
   }
 
   // Display message
   msgDiv.innerHTML = msg;
 
   // Set timer
-  vm.messageTimer = setTimeout(() => {
+  messageTimer = setTimeout(() => {
     msgDiv.innerHTML = '';
   }, timeMs);
 }
@@ -181,16 +188,16 @@ function handleKeyDown(e) {
 
   // Enter key
   if (keycode === 13) {
-    vm.evaluateInput();
+    evaluateInput();
   }
 
   // Up arrow
   // Replace input text with last entered command
   else if (keycode === 38) {
-    if (vm.lastEnteredCommand !== '') {
+    if (lastEnteredCommand !== '') {
       let input = document.querySelector('#input');
       input.focus();
-      input.value = vm.lastEnteredCommand;
+      input.value = lastEnteredCommand;
       // Put cursor at end of text
       setTimeout(() => {
         input.setSelectionRange(input.value.length, input.value.length);
@@ -206,12 +213,12 @@ function fetchGist(gistID) {
     if (xhr.readyState == 4 && xhr.status == 200) {
       let files = JSON.parse(xhr.responseText).files;
       if (files.length > 1) {
-        vm.displayMessage('Error: Multiple files found in gist. Please use a gist with only one file.', 5000);
+        displayMessage('Error: Multiple files found in gist. Please use a gist with only one file.', 5000);
         return;
       }
       let gistText = files[Object.keys(files)[0]].content;
       config.gistID = gistID;
-      vm.updateConfig(gistText);
+      updateConfig(gistText);
     }
   }
   xhr.open('GET', url, true);
@@ -223,7 +230,7 @@ function updateConfig(configString) {
   try {
     config = JSON.parse(configString);
   } catch (err) {
-    vm.displayMessage('Error parsing config, see console for details', 5000);
+    displayMessage('Error parsing config, see console for details', 5000);
     console.log(err);
     return;
   }
@@ -232,16 +239,466 @@ function updateConfig(configString) {
     config[setting] = config[setting];
   }
 
-  vm.saveConfig();
-  vm.setColors();
-  vm.displayMessage('Config imported', 5000);
+  saveConfig();
+  setColors();
+  displayMessage('Config imported', 5000);
 }
 
-function isURL(url) {
+function checkIfURL(url) {
   if (/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(url)) {
     if (!url.includes(' ')) {
       return true;
     }
   }
   return false;
+}
+
+const commands = {
+  // Set
+  'set': (args) => {
+    // Validate hex color values.
+    // #EBEBEB is valid, EBEBEB is not. #FFF is valid shorthand.
+    const validHex = hex => /^#[0-9a-f]{3}(?:[0-9a-f]{3})?$/i.test(hex)
+
+    switch(args[0]) {
+      // Default command
+      case 'defaultCommand':
+        // Display current value if none given
+        if (args.length === 1) {
+          displayMessage(`Default command: ${config.defaultCommand}`, 5000);
+          break;
+        }
+
+        // Check if existant command
+        if (Object.keys(commands).includes(args[1])) {
+          config['defaultCommand'] = args[1];
+          displayMessage(`Set default command to ${args[1]}`, 3000);
+        } else {
+          displayMessage(`Error: command ${args[1]} not found; default command not changed`, 10000);
+        }
+        break;
+
+      // Background color
+      case 'bgColor':
+        // Display current value if none given
+        if (args.length === 1) {
+          displayMessage(`Current background color: ${config.bgColor}`, 8000);
+          break;
+        }
+
+        // Set new background color
+        if (validHex(args[1])) {
+          config.bgColor = args[1];
+        } else {
+          displayMessage('Error: invalid hex value', 5000);
+        }
+        break;
+
+      // Text color
+      case 'textColor':
+        // Display current value if none given
+        if (args.length === 1) {
+          displayMessage(`Current background color: ${config.textColor}`, 8000);
+          break;
+        }
+
+        // Set new text color
+        if (validHex(args[1])) {
+          config['textColor'] = args[1];
+        } else {
+          displayMessage('Error: invalid hex value', 5000);
+        }
+        break;
+
+      // Always new tab
+      case 'newtab':
+      case 'alwaysNewTab':
+        // Display current value if none given
+        if (args.length === 1) {
+          let msg = `alwaysNewTab is ${(config.alwaysNewTab) ?  'on' : 'off'}`;
+          displayMessage(msg, 5000);
+          break;
+        }
+        if (args[1] === 'on') config.alwaysNewTab = true;
+        else if (args[1] === 'off') config.alwaysNewTab = false;
+        else displayMessage("Must be set to either 'on' or 'off'", 5000);
+        break;
+
+      // Clock
+      case 'clock':
+        // Display current value if none given
+        if (args.length === 1) {
+          displayMessage(`Clock is ${(config.showClock) ? 'on' : 'off'}`, 5000);
+          break;
+        }
+
+        // Turn clock on or off
+        if (args[1] === 'on') config.showClock = true;
+        else if (args[1] === 'off') config.showClock = false;
+        else displayMessage("Must be set to either 'on' or 'off'", 5000);
+        break;
+
+      // Restore defaults
+      case 'defaults':
+        localStorage.removeItem('taabSettings');
+        config = {
+          defaultCommand: 'g',
+          bgColor: '#282828',
+          textColor: '#ebdbb2',
+          showClock: false,
+          alwaysNewTab: false,
+          gistID: '',
+          links: {},
+        }
+        loadConfig();
+        break;
+
+      default:
+        displayMessage(`"${args[0]}" is not a valid setting`, 5000);
+    }
+
+    saveConfig();
+    setColors();
+  },
+
+  // Links
+  'link': (args) => {
+    switch(args.length) {
+      case 0:
+        displayMessage(`link is a builtin command<br>To search for link try g;link<br>`, 8000);
+        break;
+
+      case 1:
+        // Show links
+        if (args[0] === 'show') {
+          let links = '';
+          for (let link in config.links) {
+            links += `${link} --> ${config.links[link]}<br>`;
+          }
+          displayMessage(links, 30000);
+        }
+
+        // Print link URL if existant link
+        if (Object.keys(config.links).includes(args[0])) {
+          displayMessage(`"${args[0]}" links to ${config.links[args[0]]}`, 5000);
+        }
+        break;
+
+      case 2:
+        // Delete link
+        if (args[1] === 'delete') {
+          if (delete config.links[args[0]]) {
+            displayMessage(`Link ${args[0]} deleted`, 5000);
+          }
+        }
+
+        // Add new link
+        else {
+          // Ensure shortcut not taken
+          if (Object.keys(commands).includes(args[0]) || Object.keys(aliases).includes(args[0]) || Object.keys(config.links).includes(args[0])) {
+            displayMessage(`Shortcut already in use: ${args[0]}`, 5000);
+            return;
+          }
+
+          // Check that URL is valid
+          let url = buildURL(args[1]);
+          if (isURL(url)) {
+            config.links[args[0]] = url;
+          } else {
+            displayMessage('Invalid URL', 5000);
+            return;
+          }
+        }
+        break;
+    }
+
+    saveConfig();
+  },
+
+  // Config
+  'config': (args) => {
+    switch(args[0]) {
+      case 'export':
+        // Display config as string and select all of it
+        displayMessage(localStorage.getItem('taabSettings'), 1000 * 25);
+        window.getSelection().selectAllChildren(document.querySelector('#message'));
+        break;
+
+      case 'import':
+        updateConfig(args[1]);
+        break;
+
+      case 'fetch':
+        let gistID;
+        if (args.length > 1) {
+          try {
+            gistID = args[1].match(/([0-9A-Za-z]{32})/)[0];
+          } catch (err) {
+            displayMessage('Error: unable to parse gist ID.<br>Try entering just the 32 character ID string.', 8000);
+            return;
+          }
+        } else if (config.gistID != undefined) {
+          gistID = config.gistID;
+        } else {
+          displayMessage('Error: no gist ID', 5000);
+          break;
+        }
+        displayMessage('Fetching gist...', 2500);
+        fetchGist(gistID);
+        break;
+    }
+  },
+
+  // Help
+  'help': (args) => {
+    newTab = true;
+    redirect('https://github.com/taab-site/taab#readme');
+  },
+
+  // Google
+  'g': (args) => {
+    const url = 'https://google.com', search = '/search?q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args));
+  },
+
+  // DuckDuckGo
+  'dg': (args) => {
+    const url = 'https://duckduckgo.com', search = '/?q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args));
+  },
+
+  // Reddit
+  'r': (args) => {
+    const url = 'https://reddit.com', search = '/r/';
+    let query = (args.length > 0) ? args[0] : '';
+
+    const validSort = (sort) => {
+      return (['hot', 'new', 'rising', 'controversial', 'top', 'gilded', 'wiki', 'promoted'].includes(sort))
+    };
+    const validRange = (range) => {
+      return (['day', 'week', 'month', 'year', 'all'].includes(range))
+    };
+
+    switch(args.length) {
+      // Given nothing
+      case 0:
+        redirect(url);
+        break;
+
+      // Given a subreddit
+      case 1:
+        redirect(buildURL(url, search, args[0]));
+        break;
+
+      // Given subreddit and sort
+      case 2:
+        query += (validSort(args[1]))
+          ? '/' + args[1]
+          : '';
+        redirect(url + search + query);
+        break;
+
+      // Given subreddit, sort and range
+      case 3:
+        if (['top', 'controversial'].includes(args[1])) {
+          query += (validRange(args[2]))
+            ? '/' + args[1] + '?t=' + args[2]
+            : '';
+        } else {
+          query += (validSort(args[1]))
+            ? '/' + args[1]
+            : '';
+        }
+        redirect(url + search + query);
+        break;
+    }
+  },
+
+  // Hacker News
+  'hn': (args) => {
+    const url = 'https://news.ycombinator.com';
+    if (args.length == 0) {
+      redirect(url);
+    } else {
+      switch(args[0]) {
+        case 'new':
+          redirect(url + '/newest');
+          break;
+
+        case 'comments':
+          redirect(url + '/newcomments');
+          break;
+
+        case 'show':
+          redirect(url + '/show');
+          break;
+
+        case 'ask':
+          redirect(url + '/ask');
+          break;
+
+        case 'jobs':
+          redirect(url + '/jobs');
+          break;
+
+        case 'submit':
+          redirect(url + '/submit');
+          break;
+      }
+    }
+  },
+
+  // Youtube
+  'y': (args) => {
+    const url = 'https://youtube.com', search = '/results?search_query=';
+    if (args.length == 0) {
+      redirect(url);
+    } else {
+      if (['subs', 's'].includes(args[0]))
+        redirect(url + '/feed/subscriptions');
+      else
+        redirect(buildURL(url, search, args[0]));
+    }
+  },
+
+  // Wikipedia
+  'w': (args) => {
+    const url = 'https://wikipedia.org', search = '/w/index.php?title=Special:Search&search=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // GitHub
+  'gh': (args) => {
+    const url = 'https://github.com', search = '/';
+    if (args.length == 0) redirect(url)
+    else redirect(url + search + args.join(''));
+  },
+
+  // GitHub Gist
+  'gist': (args) => {
+    const url = 'https://gist.github.com', search = '/';
+    if (args.length == 0) redirect(url)
+    else redirect(url + search + args.join(''));
+  },
+
+  // Wolfram Alpha
+  'wa': (args) => {
+    const url = 'http://wolframalpha.com', search = '/input/?i=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Netflix
+  'n': (args) => {
+    const url = 'https://netflix.com', search = '/search?q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Internet Movie Database
+  'imdb': (args) => {
+    const url = 'http://imdb.com', search = '/find?s=all&q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Google Maps
+  'gm': (args) => {
+    const url = 'https://google.com/maps', search = '/search/';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Google Drive
+  'gd': (args) => {
+    const url = 'https://drive.google.com', search = '/drive/search?q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Google Calendar
+  'gc': (args) => {
+    redirect('https://calendar.google.com');
+  },
+
+  // Google Images
+  'img': (args) => {
+    const url = 'https://google.com', search = '/search?tbm=isch&q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Google Inbox
+  'i': (args) => {
+    const url = 'https://inbox.google.com', search = '/search/';
+    if (args.length == 0) {
+      redirect(url);
+    } else {
+      if (args[0] === 'snoozed')
+        redirect(url + '/snoozed')
+      else if (args[0] === 'done')
+        redirect(url + '/done')
+      else
+        redirect(buildURL(url, search, args.join(' ')));
+    }
+  },
+
+  // Google Keep
+  'k': (args) => {
+    const url = 'https://keep.google.com', search = '/#search/text=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Trello
+  'tr': (args) => {
+    const url = 'https://trello.com', search = '/search?q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Dictionary
+  'dict': (args) => {
+    const url = 'http://dictionary.com', search = '/browse/';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Thesaurus
+  'thes': (args) => {
+    const url = 'http://thesaurus.com', search = '/browse/';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Amazon
+  'a': (args) => {
+    const url = 'https://amazon.com', search = '/s/?field-keywords=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Node package manager
+  'npm': (args) => {
+    const url = 'https://npmjs.org', search = '/search?q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Python package index
+  'pypi': (args) => {
+    const url = 'https://pypi.org', search = '/search/?q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  },
+
+  // Stack Overflow
+  'so': (args) => {
+    const url = 'https://stackoverflow.com', search = '/search?q=';
+    if (args.length == 0) redirect(url)
+    else redirect(buildURL(url, search, args.join(' ')));
+  }
 }
