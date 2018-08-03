163797c33b8f5d7ba5d3c5c2855b89d065fa7ff0
Kory Schneider <kory.schneider@gmail.com>
2018-08-02 17:16:45 -0700

    Fix title

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'taab',
+      title: 'tab',
       template: __dirname + '/src/template.html',
       filename: __dirname + '/index.html',
       inlineSource: '.(js|css)$',
